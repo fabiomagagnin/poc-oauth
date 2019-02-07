2fdbee57f9669498d5f556ba5de4ada50e2ac7fe
Fábio Magagnin <fabio.magagnin@meta.com.br>
2019-02-07 15:10:39 -0200

    initial project

---
+++ src/main/java/com/example/pocoauth/PocOauthApplication.java
@@ -0,0 +1,14 @@
+package com.example.pocoauth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PocOauthApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PocOauthApplication.class, args);
+	}
+
+}
+
